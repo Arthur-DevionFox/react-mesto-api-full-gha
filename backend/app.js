e2bd961ad278f2b0e778ef1f64119c068b751288
Arthur-DevionFox <blackberserkfox@mail.ru>
2023-07-13 23:30:28 +0300

    Подготовить проект к выкладке

--- backend/app.js
+++ backend/app.js
@@ -38,8 +38,8 @@ app.get('/crash-test', () => {
   }, 0);
 });
 
-app.post('/signup', validateUserCreate, createUser);
-app.post('/signin', validateUserAuth, userLogin);
+app.post('/sign-up', validateUserCreate, createUser);
+app.post('/sign-in', validateUserAuth, userLogin);
 
 app.use(auth);
 
